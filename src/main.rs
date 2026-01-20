47b184bf1b4f2fdd8b9894ae9014a53b2dd389a7
Endybits <endyb.dev@gmail.com>
2026-01-19 23:40:15 -0500

    Implement data generation and retrieval for training dataset

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod simple_regression;
 
 use crate::simple_regression::linear_regression::train;
+use crate::simple_regression::data::get_training_data;
 
 fn main() {
     println!("Hello, world!");
     train();
+    let dataset = get_training_data();
+
+    println!("Training data:");
+    for (x, y) in dataset.x_train.iter().zip(dataset.y_train.iter()) {
+        println!("Area: {:.2} m2, Price: {:.2} millions", x, y);
+    }
 }
---
+++ src/simple_regression/data.rs
@@ -0,0 +1,40 @@
+use rand::Rng;
+pub struct Dataset {
+    pub x_train: Vec<f64>,
+    pub y_train: Vec<f64>,
+}
+
+// 1. Estuctura acoplada
+// Simulamos la llegada de datos como de una database o archivo CSV
+
+struct RawRecord {
+    area: f64,
+    price: f64,
+}
+
+fn generate_mock_records(count: usize) -> Vec<RawRecord> {
+    let mut records = Vec::new();
+    let mut rng = rand::rng();
+
+    for _ in 0..count {
+        // Simulamos area entre 34 y 250 m2
+        let area: f64 = rng.random_range(34.0..250.0);
+        // Precio igual a area por 4.5 millones + ruido aleatorio + bias
+        let noise: f64 = rand::random::<f64>() - 0.5;
+        let price = area * 4.5 + noise + 20.0;
+        records.push(RawRecord { area, price });
+    }
+    records
+}
+
+
+pub fn get_training_data() -> Dataset {
+    // Simulamos la carga de datos
+    let raw_data = generate_mock_records(10);
+
+    // ETL: Extraer las características y etiquetas
+    let x_train: Vec<f64> = raw_data.iter().map(|record| record.area).collect();
+    let y_train: Vec<f64> = raw_data.iter().map(|record| record.price).collect();
+
+    Dataset { x_train, y_train }
+}
